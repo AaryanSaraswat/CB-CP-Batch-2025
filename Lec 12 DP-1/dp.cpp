d19f85b883f08143a8c9bf85fe1363a0e55c6b96
aaryanCompro <aaryan.saraswat@comprotechnologies.com>
2025-06-21 20:29:16 +0530

    Added DP Lec1

--- Lec 12 DP-1/dp.cpp
+++ Lec 12 DP-1/dp.cpp
@@ -3,6 +3,21 @@ using namespace std;
 
 int main()
 {
-    
+    int n; cin>>n;
+
+    int dp[n+1]; 
+
+    // smallest sub problem jiska ans pata h
+    dp[0]=0;
+    dp[1]=1;
+
+    for(int k=2;k<=n;k++){
+        // transition 
+        dp[k]= dp[k-1] + dp[k-2];
+    }
+
+    // final ans
+    cout<<dp[n]<<endl;
+
     return 0;
 }
