d7b54a4cdd14fd5e4cec422f59ead62892c07add
aaryanCompro <aaryan.saraswat@comprotechnologies.com>
2025-07-27 07:14:49 +0530

    Added Lecture 20

---
+++ Lec 20 Trees/dfs.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define loop(i, a, n) for (lli i = (a); i < (n); ++i)
+#define loopD(i, a, n) for (lli i = (a); i >= (n); --i)
+#define all(c) (c).begin(), (c).end()
+#define rall(c) (c).rbegin(), (c).rend()
+#define sz(a) ((int)a.size())
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define endl '\n'
+#define fastio std::ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+#define pb push_back
+#define pp pop_back()
+#define fi first
+#define si second
+#define v(a) vector<int>(a)
+#define vv(a) vector<vector<int>>(a)
+#define present(c, x) ((c).find(x) != (c).end())
+#define set_bits __builtin_popcountll
+#define MOD 1000000007
+#define int long long
+
+typedef long long lli;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<lli, lli> pll;
+typedef pair<int, int> pii;
+typedef unordered_map<int, int> umpi;
+typedef map<int, int> mpi;
+typedef vector<pii> vp;
+typedef vector<lli> vll;
+typedef vector<vll> vvll;
+
+vi in(100),out(100);
+int timme = 1;
+
+void dfs(int node,int parent, vvi &adj){
+    in[node]=timme++; // in[node]=time; time++;
+    for(int ch:adj[node]){
+        if(ch==parent) continue;
+        dfs(ch,node,adj);
+    }
+    out[node]=timme++;
+}
+
+void solve(){
+    int n; cin>>n;
+    vvi adj(n+1,vi());
+
+    for(int i=1;i<n;i++){ // n-1 edges
+        int u,v; cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    dfs(1,0,adj);
+}
+
+int32_t main()
+{
+    fastio;
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        
+    }
+    return 0;
+}
