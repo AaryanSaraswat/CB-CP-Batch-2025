93f957e588c495c27059f726a0dc50d8aff7fd9e
aaryanCompro <aaryan.saraswat@comprotechnologies.com>
2025-08-02 07:11:37 +0530

    Added Lecture 21

---
+++ Lec 21 Trees/LCA.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define loop(i, a, n) for (lli i = (a); i < (n); ++i)
+#define loopD(i, a, n) for (lli i = (a); i >= (n); --i)
+#define all(c) (c).begin(), (c).end()
+#define rall(c) (c).rbegin(), (c).rend()
+#define sz(a) ((int)a.size())
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define endl '\n'
+#define fastio std::ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+#define pb push_back
+#define pp pop_back()
+#define fi first
+#define si second
+#define v(a) vector<int>(a)
+#define vv(a) vector<vector<int>>(a)
+#define present(c, x) ((c).find(x) != (c).end())
+#define set_bits __builtin_popcountll
+#define MOD 1000000007
+#define int long long
+
+typedef long long lli;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<lli, lli> pll;
+typedef pair<int, int> pii;
+typedef unordered_map<int, int> umpi;
+typedef map<int, int> mpi;
+typedef vector<pii> vp;
+typedef vector<lli> vll;
+typedef vector<vll> vvll;
+
+int dp[100010][18], level[100010];
+
+void dfs(int node,int parent,vvi &adj, int lvl){
+    level[node] = lvl;
+    dp[node][0]= parent;
+    loop(x,1,18){
+        dp[node][x] = dp[dp[node][x-1]][x-1];
+    }
+    for(auto ch:adj[node]){
+        if(ch==parent) continue;
+        dfs(ch,node,adj,lvl+1);
+    }
+}
+
+int kthPar(int node,int k){
+    int par = node;
+    for(int bit=17;bit>=0;bit--){
+        if(k&(1<<bit)) par = dp[par][bit];
+    }
+    return par;
+}
+
+int lca(int u,int v){
+    // same level par lana hai
+    if(level[u]>level[v]) swap(u,v);
+    int k  = level[v] - level[u];
+    v = kthPar(v,k);
+
+    // check if nodes are same 
+    if(u==v) return u;
+
+    // parents par iterate and move on diff parents
+    loopD(x,17,0){
+        if(dp[u][x]!=dp[v][x]) u = dp[u][x] , v= dp[v][x];
+    }
+
+    // return lca
+    return dp[u][0]; // dp[v][0]
+}
+
+void solve(){
+    int n; cin>>n;
+    vvi adj(n+1,vi());
+
+    for(int i=1;i<n;i++){ // n-1 edges
+        int u,v; cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    memset(dp, 0, sizeof(dp));
+
+    // Start DFS from root node (assuming node 1 is the root)
+    dfs(1, 0, adj, 1);
+
+    int q;
+    cin >> q;
+    while (q--) {
+        int node, k;
+        cin >> node >> k;
+        cout << kthPar(node, k) << endl;
+    }
+   
+}
+
+int32_t main()
+{
+    fastio;
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        
+    }
+    return 0;
+}
---
+++ Lec 21 Trees/eularTour2.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define loop(i, a, n) for (lli i = (a); i < (n); ++i)
+#define loopD(i, a, n) for (lli i = (a); i >= (n); --i)
+#define all(c) (c).begin(), (c).end()
+#define rall(c) (c).rbegin(), (c).rend()
+#define sz(a) ((int)a.size())
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define endl '\n'
+#define fastio std::ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+#define pb push_back
+#define pp pop_back()
+#define fi first
+#define si second
+#define v(a) vector<int>(a)
+#define vv(a) vector<vector<int>>(a)
+#define present(c, x) ((c).find(x) != (c).end())
+#define set_bits __builtin_popcountll
+#define MOD 1000000007
+#define int long long
+
+typedef long long lli;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<lli, lli> pll;
+typedef pair<int, int> pii;
+typedef unordered_map<int, int> umpi;
+typedef map<int, int> mpi;
+typedef vector<pii> vp;
+typedef vector<lli> vll;
+typedef vector<vll> vvll;
+
+vi adj[101];
+int level[101],flat[1100];
+int idx=0;
+
+unordered_map<int,pii> occurences;
+
+void dfs(int node,int parent){
+    level[node] = level[parent] + 1;
+    occurences[node].fi=idx;
+    flat[idx]=node; idx++; // first occurence
+    for(auto ch:adj[node]){
+        if(ch==parent)continue;
+        dfs(ch,node);
+    } 
+    occurences[node].si=idx;
+    flat[idx]=node; idx++; // last occurence
+}
+
+int32_t main()
+{
+    fastio;
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+        
+    }
+    return 0;
+}
